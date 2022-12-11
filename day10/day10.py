90c87eae46cfc681be1a17157f8ee4560fe52026
0xHanan <nvm>
2022-12-11 12:01:20 +0200

    Day 10 part 1 done

--- day10/day10.py
+++ day10/day10.py
@@ -3,9 +3,26 @@ from sys import argv
 part = int(argv[1])
 
 with open("day10/input", "r") as f:
-    data = f.read()
+    data = f.read().splitlines()
 
 if part == 1:
-    pass
+    OPS = {"addx":2, "noop":1}
+    total_cycles = 0
+    reg_X = 1
+    signal_strength = 0
+    signal_calc = 60
+    for i in data:
+        op = i.split()
+        operand = int(op[1]) if len(op) > 1 else None
+        for c in range(OPS[op[0]]):
+            total_cycles += 1
+            if total_cycles == 20:
+                signal_strength = reg_X * total_cycles
+            elif total_cycles == signal_calc:
+                signal_strength += (reg_X * total_cycles)
+                signal_calc += 40
+        if operand: 
+            reg_X += operand
+    print(signal_strength)
 else:
     pass
