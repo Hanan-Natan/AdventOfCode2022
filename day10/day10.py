65bff72717b5b4d93348c3ca50e7a52be2df586e
0xHanan <nvm>
2022-12-11 14:13:41 +0200

    day 10 done

--- day10/day10.py
+++ day10/day10.py
@@ -25,4 +25,19 @@ if part == 1:
             reg_X += operand
     print(signal_strength)
 else:
-    pass
+    OPS = {"addx":2, "noop":1}
+    total_cycles = 0
+    reg_X = 1
+    crt_screen = [["." for _ in range(40)] for _ in range(6)]
+    print(len(data))
+    signal_strength = 0
+    signal_calc = 60
+    for i in data:
+        op = i.split()
+        for c in range(OPS[op[0]]):
+            if ((total_cycles-(40*(total_cycles//40)))-1 <= reg_X <= (total_cycles-(40*(total_cycles//40)))+1):
+                crt_screen[total_cycles//40][(total_cycles-(40*(total_cycles//40)))] = "#"
+            total_cycles += 1
+        reg_X += int(op[1]) if len(op) > 1 else 0
+    for l in crt_screen:
+        print(''.join(l))
