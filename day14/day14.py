6c2dcf5e32ef702c1af120ee2662641142e27c0b
0xHanan <nvm>
2022-12-23 13:34:51 +0200

    day 14 part 2 done

--- day14/day14.py
+++ day14/day14.py
@@ -15,15 +15,16 @@ col_max_height = 0
 def print_graph(data):
     c = 0
     while True:
-        if c == 10:
+        # if c == col_max_height+1: # for part 1 only
+        if c == col_max_height+3:
             break
         l = ""
-        for idx, line in data.items():
-            if line[c] == 0:
+        for line in sorted(data):
+            if data[line][c] == 0:
                 l += "."
-            elif line[c] == 1:
+            elif data[line][c] == 1:
                 l += "#"
-            elif line[c] == -1:
+            elif data[line][c] == -1:
                 l += "o"
             else:
                 l += "."
@@ -67,59 +68,29 @@ def check_move(table, col, row):
     table[col][row] = -1
     # print_graph(table)
 
-# def move(table,point):
-#     start = 500
-#     DIR = [(0, 0), (0,-1), (0, 1)] # down, left, right
-#     moved = False
-#     move_to = (0,0)
-#     while True:
-#         # check down
-#         down = point+DIR[0][1]
-#         if down != 10:
-#             print("Testing down", start, down, table[start][down])
-#             if table[start][down] == 0:
-#                 start, point = start, down
-#                 move_to= (start, down)
-#                 moved = True
-#                 continue
-#         if move_to[0] == 497:
-#             print("Show me next", move_to)
-#             print_graph(table)
-#         left, down = start+DIR[1][1], point+DIR[1][0]
-#         if down != 10:
-#             print("Testing left", left, down, table[left][down])
-#             if left < col_start_val:
-#                 print(f"left OOB at {d}")
-#                 print(left, col_start_val)
-#                 print_graph(table)
-#                 exit(0)
-#             if table[left][down] == 0:
-#                 DIR[0] = (0,1) # we set it here so taht first time we check only 0
-#                 start, point = left, down+1
-#                 move_to= (left, down)
-#                 moved = True
-#                 continue
-#         # try right
-#         right, down = start+DIR[2][1], point+DIR[2][0] 
-#         if down != 10:
-#             print("Testing right", right, down, table[right][down])
-#             if right > col_max_val:
-#                 print(f"right OOB at {d}")
-#                 print_graph(table)
-#                 print(right, col_start_val)
-#                 exit(0)
-#             if table[right][down] == 0:
-#                 DIR[0] = (0,1) # we set it here so taht first time we check only 0
-#                 move_to= (right, down)
-#                 start, point = right, down+1
-#                 moved = True
-#                 continue
-#         break
-#     if moved:
-#         # print("Settle at",move_to)
-#         table[move_to[0]][move_to[1]] = -1
+def check_move_p2(table, col, row):
+    global col_start_val, col_max_val
+    while True:
+        if table[col][row+1] == 0:
+            row += 1
+            continue
+        elif table[col-1][row+1] == 0:
+            col -= 1
+            row += 1
+            if col-1 < col_start_val:
+                table.update({col-1: [0 for _ in range(col_max_height + 2)] + [1]})
+                col_start_val = col-1
+            continue
+        elif table[col+1][row+1] == 0:
+            col += 1
+            row += 1
+            if col+1 > col_max_val:
+                table.update({col+1: [0 for _ in range(col_max_height + 2)] + [1]})
+                col_max_val = col+1
+            continue
+        break
+    table[col][row] = -1
 
-#     return moved
 
 # Part 1 took me really long time. I didn't focus on a whole simple solution and always built another little fix that comlicated the solution and made it more complicated.
 if part == 1:
@@ -134,10 +105,7 @@ if part == 1:
         for i in range(1, len(l)):
             start, end = min([l[i-1], l[i]]), max([l[i-1], l[i]])
             for j in range(start[0], end[0]+1):
-                # print(j)
                 for k in range(start[1], end[1]+1):
-                    # print(k)
-                    print(j, k)
                     table[j][k] = 1
     # print(table)
     # sand falls
@@ -148,4 +116,28 @@ if part == 1:
         print("Step", count)
         count +=1
 else:
-    pass
+    data = parse(data)
+    table = {}
+    for i in range(col_start_val, col_max_val+1):
+        # generate table with colum for each
+        table[i]=[0 for _ in range(col_max_height+2)] + [1]
+
+    # draw
+    for l in data:
+        for i in range(1, len(l)):
+            start, end = min([l[i-1], l[i]]), max([l[i-1], l[i]])
+            for j in range(start[0], end[0]+1):
+                for k in range(start[1], end[1]+1):
+                    table[j][k] = 1
+
+    start = 500
+    count = 0
+    while True:
+        if table[start][0] == -1:
+            print("Step", count)
+            break
+        check_move_p2(table, start, 0)
+        count +=1
+        # if count == 96:
+        #     break
+    # print_graph(table)
